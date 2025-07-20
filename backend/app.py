e453c83abaca5d9b8df34cb1d2552d31ad4efa63
AndrewDeng1 <bndarw2006@gmail.com>
2025-07-20 01:05:39 -0400

    get user socials working

--- backend/app.py
+++ backend/app.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 from datetime import datetime
 
 # Import endpoint functions
-from endpoints.user import health, create_or_get_user
+from endpoints.user import health, create_or_get_user, get_user_socials
 from endpoints.rizzume import create_or_upload_rizzume, get_user_rizzume, get_all_rizzumes
 from endpoints.ribbon import ping_ribbon, create_or_update_interview_flow, create_or_update_interview, get_all_interviews
 from endpoints.application import create_application, update_application, get_applications_for_interviewer_and_update_status, get_applications_for_applicant_and_update_status, update_interviewer_decision
@@ -45,6 +45,7 @@ app.route('/api/get_applications_for_interviewer_and_update_status', methods=['P
 app.route('/api/get_applications_for_applicant_and_update_status', methods=['POST'])(get_applications_for_applicant_and_update_status)
 app.route('/api/get_all_interviews', methods=['GET'])(get_all_interviews)
 app.route('/api/update_interviewer_decision', methods=['POST'])(update_interviewer_decision)
+app.route('/api/get_user_socials', methods=['POST'])(get_user_socials)
 
 @app.route('/')
 def home():
