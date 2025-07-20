a51369dab736ba70210d4a2d3801efdf54370f4b
Song Eric Yu Li <ericli3690@gmail.com>
2025-07-20 02:30:34 -0400

    fixing up dashboard more

--- frontend/waterloodates/src/app/dashboard/page.tsx
+++ frontend/waterloodates/src/app/dashboard/page.tsx
@@ -57,11 +57,6 @@ export default withPageAuthRequired(function DashboardPage({ user }) {
                   setOutgoingApplications(resp.applications as Application[]);
                 }
               })
-              .then((resp) => {
-                if (resp && resp.applications) {
-                  setOutgoingApplications(resp.applications as Application[]);
-                }
-              })
               .catch(console.error);
 
             if (data.wingman_created) {
